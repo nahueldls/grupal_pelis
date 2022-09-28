51de0d9abd7b5117d7ba9690414362e74eb1d30c
Franco Pelúa <erekkles@gmail.com>
2022-09-27 22:50:24 -0300

    Fix bug with genre filtering

--- JAPFLIX/js/index.js
+++ JAPFLIX/js/index.js
@@ -4,14 +4,14 @@ let conteiner = document.getElementById("content");
 
 const pelis = await fetch(`https://japceibal.github.io/japflix_api/movies-data.json`)
 .then( response => response.json())
-// console.log(pelis[0].genres[0].name);
+// console.log(pelis[0].genres[0].id);
 
 
 function showContent(list) {
     for (let i = 0; i < list.length; i++) {
         conteiner.innerHTML += `
         <p  class="listado">${list[i].title}</p>
-        <p class="desc">${list[i].tagline}</p>`
+        <p class="desc" style="color:darkgrey">${list[i].tagline}</p>`
         
         // ---Estrellas---en función de average
     }
@@ -23,17 +23,24 @@ de title o genres o tagline u overview. A TENER EN CUENTAA*/
 let i = 0;
 buscador_button.addEventListener("click", () => {
     if ( conteiner.innerHTML == "") {
-        if ( input.value != ""){
-            // showContent(pelis.filter( filtrados => filtrados.title.toLowerCase().includes(input.value))) || 
-            // showContent(pelis.filter( filtrados => filtrados.tagline.toLowerCase().includes(input.value))) ||
-            // showContent(pelis.filter( filtrados => filtrados.overview.toLowerCase().includes(input.value)))
-            for (let i = 0; i < pelis.length; i++) {
-                pelis.forEach(peli => { //recorrer
-                    // console.log(peli.genres.name)
-                    showContent(pelis[i].genres[i].name.filter( filtrados => filtrados.genres[i].name.toLowerCase().includes(input.value)))
-                });
+        if ( input.value !== ""){
+            const conditionForFilteringGenre = (genre) => genre.name.toLowerCase().includes(input.value.toLowerCase());
+
+            showContent(pelis.filter( filtrados => filtrados.title.toLowerCase().includes(input.value))) || 
+            showContent(pelis.filter( filtrados => filtrados.tagline.toLowerCase().includes(input.value))) ||
+            showContent(pelis.filter( filtrados => filtrados.overview.toLowerCase().includes(input.value))) || 
+            showContent(pelis.filter((peli) => peli.genres.some((genre) => conditionForFilteringGenre(genre))))
+
+           
+
+            
+            // for (let i = 0; i < pelis.length; i++) {
+            //     pelis.forEach(peli => { //recorrer
+            //         console.log(peli)
+            //         showContent(pelis.filter( filtrados => filtrados.genres[i].name.toLowerCase().includes(input.value)))
+            //     });
                 
-            } 
+            // } 
         }
     } else {
         conteiner.innerHTML = ""

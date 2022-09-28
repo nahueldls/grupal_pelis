af818fc11ed6af37ffe81ba67bccc7096d27d631
Nahuel De Los Santos <nahueldelossantos646@gmail.com>
2022-09-27 22:21:19 -0300

    add a a js

---
+++ index.js
@@ -0,0 +1,68 @@
+const input = document.getElementById("inputBuscar");
+const buscador_button = document.getElementById("btnBuscar");
+let conteiner = document.getElementById("content");
+
+const pelis = await fetch(`https://japceibal.github.io/japflix_api/movies-data.json`)
+.then( response => response.json())
+// console.log(pelis[0].genres[0].name);
+
+
+function showContent(list) {
+    for (let i = 0; i < list.length; i++) {
+        conteiner.innerHTML += `
+        <p  class="listado">${list[i].title}</p>
+        <p class="desc">${list[i].tagline}</p>`
+        
+        // ---Estrellas---en función de average
+    }
+}
+
+/* Cuando el usuario presiona el botón buscar, y si previamente ingresó algún valor en el campo de búsqueda, 
+deberán mostrarle un listado con las películas que coincidan con dicha búsqueda en sus atributos 
+de title o genres o tagline u overview. A TENER EN CUENTAA*/
+let i = 0;
+buscador_button.addEventListener("click", () => {
+    if ( conteiner.innerHTML == "") {
+        if ( input.value != ""){
+            // showContent(pelis.filter( filtrados => filtrados.title.toLowerCase().includes(input.value))) || 
+            // showContent(pelis.filter( filtrados => filtrados.tagline.toLowerCase().includes(input.value))) ||
+            // showContent(pelis.filter( filtrados => filtrados.overview.toLowerCase().includes(input.value)))
+            for (let i = 0; i < pelis.length; i++) {
+                pelis.forEach(peli => { //recorrer
+                    // console.log(peli.genres.name)
+                    showContent(pelis[i].genres[i].name.filter( filtrados => filtrados.genres[i].name.toLowerCase().includes(input.value)))
+                });
+                
+            } 
+        }
+    } else {
+        conteiner.innerHTML = ""
+        //showContent(pelis.filter( filtrados => filtrados.title.toLowerCase().includes(input.value)))
+    }
+})
+
+/* Cuando el usuario presiona el botón buscar, y si previamente ingresó algún valor en el campo de búsqueda, 
+deberán mostrarle un listado con las películas que coincidan con dicha búsqueda en sus atributos 
+de title o genres o tagline u overview. A TENER EN CUENTAA*/
+
+/* tengo que hacer que la busqueda coincida con el title o genres o tagline u overview*/
+
+// let h = "";  
+// let j = 0;
+// for (let i = 0; i < pelis[i].genres[i].length; i++) {          
+//     h = pelis[i].genres[i].name;
+//     console.log(h)
+// 
+// let h = "";
+//  for (let i = 0; i < pelis.length; i++) {
+    // pelis.forEach(peli => {
+        // if ( peli.genres[i].name == "action") {
+        // console.log(peli.genres)
+        // }
+    // });
+    // h += pelis[i].genres[i].name
+    // console.log(h)
+    
+// } 
+
+// console.log(pelis[1].genres[0])

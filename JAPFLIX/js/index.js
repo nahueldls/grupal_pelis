d0c9c2f7b6222d3ed38d1ee8104d3adf4cc9fa87
Nahuel De Los Santos <nahueldelossantos646@gmail.com>
2022-10-01 16:43:54 -0300

    pelis

--- JAPFLIX/js/index.js
+++ JAPFLIX/js/index.js
@@ -4,18 +4,51 @@ let conteiner = document.getElementById("content");
 
 const pelis = await fetch(`https://japceibal.github.io/japflix_api/movies-data.json`)
 .then( response => response.json())
-// console.log(pelis[0].genres[0].id);
+console.log( );
 
-
+let u = 0;
+let stars = ""
+let divforstars = document.getElementById("stars")
 function showContent(list) {
     for (let i = 0; i < list.length; i++) {
-        conteiner.innerHTML += `
-        <p  class="listado">${list[i].title}</p>
-        <p class="desc" style="color:darkgrey">${list[i].tagline}</p>`
+         // ---Estrellas---en función de average
+         while ( u < list[i].vote_average) {
+            stars += `<span class="fa fa-star checked"></span>`
+            u += 2
+        }
+        if ( u < 10) {
+            while ( u < 10) {
+                stars += `<span class="fa fa-star"></span>`
+                u +=2
+            }
+        }
+        u = 0
+        conteiner.innerHTML += divforstars
+        conteiner.innerHTML += ` 
+        <p  class="listado id">${list[i].title}</p>
+        <p class="desc" style="color:darkgrey">${list[i].tagline}${stars}</p>`
+        u = 0
+        divforstars = ""
+        stars = ""
         
-        // ---Estrellas---en función de average
+        // // ---Estrellas---en función de average
+        // while ( u < list[i].vote_average) {
+        //     stars += `<span class="fa fa-star checked"></span>`
+        //     u += 1
+        // }
+        // divforstars += stars
+        // u = 0
+        // conteiner.innerHTML += divforstars
+        // divforstars = ""
+        // stars = ""
+
     }
+    // let u;
+    // for ( u = 0; u < list.length; u++) {
+
+    // }
 }
+
 
 /* Cuando el usuario presiona el botón buscar, y si previamente ingresó algún valor en el campo de búsqueda, 
 deberán mostrarle un listado con las películas que coincidan con dicha búsqueda en sus atributos 
@@ -73,3 +106,54 @@ de title o genres o tagline u overview. A TENER EN CUENTAA*/
 // } 
 
 // console.log(pelis[1].genres[0])
+let canvas = document.getElementById("offcanvasExample");
+let class_object = document.getElementsByClassName("id");
+
+function clickForData() {
+
+
+
+    for (let i = 0; i < pelis.length; i++) {
+
+    canvas.innerHTML = `
+    <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">${pelis[i].title}</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+    ${pelis[i].overview} 
+    </div>
+    <div class="dropdown mt-3">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+          More
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item">Year</a></li>
+        <li><a class="dropdown-item">Runtime</a></li>
+        <li><a class="dropdown-item">Budget</a></li>
+        <li><a class="dropdown-item">Revenue</a></li>
+      </ul>
+    </div>
+  </div>`
+    }
+}
+conteiner.addEventListener("click",clickForData)
+
+// let class_object = document.getElementsByClassName("id");
+// function clickForData() {
+//     for (let i = 0; i < pelis.length; i++) {
+//         console.log(class_object[i])
+        
+//     }
+// }
+
+buscador_button.addEventListener("click", clickForData)
+
+
+// function saveID() {
+//     for ( let i = 0; i < pelis.length; i++) {
+//         console.log(pelis[i].id)
+//     }
+// }
+// saveID()
